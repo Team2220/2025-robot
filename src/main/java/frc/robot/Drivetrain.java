d7573e924c434e6025c4101375808609031c0350
notJithin <123132870+notJithin@users.noreply.github.com>
2025-01-09 19:21:01 -0600

    New Robot Template
    NEWWWWWWWWWWW

---
+++ src/main/java/frc/robot/Drivetrain.java
@@ -0,0 +1,148 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import static edu.wpi.first.units.Units.Radians;
+import static edu.wpi.first.units.Units.RadiansPerSecond;
+import static edu.wpi.first.units.Units.Volts;
+
+import org.littletonrobotics.junction.Logger;
+
+import edu.wpi.first.math.estimator.DifferentialDrivePoseEstimator;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
+import edu.wpi.first.math.kinematics.DifferentialDriveWheelPositions;
+import edu.wpi.first.math.kinematics.DifferentialDriveWheelSpeeds;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+/** Represents a differential drive style drivetrain. */
+public class Drivetrain extends SubsystemBase {
+  public static final double kMaxSpeed = 3.0; // meters per second
+  public static final double kMaxAngularSpeed = 2 * Math.PI; // one rotation per second
+
+  private static final double kTrackWidth = 0.381 * 2; // meters
+  private static final double kWheelRadius = 0.0508; // meters
+
+  private static final Pose2d kInitialPose = new Pose2d(2, 6, Rotation2d.fromDegrees(0));
+
+  // IO interfaces
+  private final DriveSideIO leftDriveIO;
+  private final DriveSideIO rightDriveIO;
+  private final GyroIO gyroIO;
+
+  // IO inputs
+  private DriveSideIOInputsAutoLogged leftDriveIOInputs = new DriveSideIOInputsAutoLogged();
+  private DriveSideIOInputsAutoLogged rightDriveIOInputs = new DriveSideIOInputsAutoLogged();
+  private GyroIOInputsAutoLogged gyroIOInputs = new GyroIOInputsAutoLogged();
+
+  private final DifferentialDriveKinematics m_kinematics = new DifferentialDriveKinematics(kTrackWidth);
+
+  private final DifferentialDrivePoseEstimator poseEstimator = new DifferentialDrivePoseEstimator(
+      m_kinematics, Rotation2d.kZero, 0, 0, kInitialPose);
+
+  private Rotation2d rawGyroRotation = Rotation2d.kZero;
+
+  private DifferentialDriveWheelPositions lastWheelPositions = new DifferentialDriveWheelPositions(0, 0);
+
+  /**
+   * Constructs a differential drive object. Sets the encoder distance per pulse
+   * and resets the
+   * gyro.
+   */
+  public Drivetrain(DriveSideIO left, DriveSideIO right, GyroIO gyro) {
+    leftDriveIO = left;
+    rightDriveIO = right;
+    gyroIO = gyro;
+
+    gyroIO.reset();
+
+    leftDriveIO.updateInputs(leftDriveIOInputs);
+    rightDriveIO.updateInputs(rightDriveIOInputs);
+    gyroIO.updateInputs(gyroIOInputs);
+
+    var wheelPositions = getDifferentialDrivePositions();
+
+    poseEstimator.updateWithTime(0, rawGyroRotation, wheelPositions.leftMeters, wheelPositions.rightMeters);
+  }
+
+  /**
+   * Sets the desired wheel speeds.
+   *
+   * @param speeds The desired wheel speeds.
+   */
+  public void setSpeeds(DifferentialDriveWheelSpeeds speeds) {
+    var leftAngularVelocity = RadiansPerSecond.of(speeds.leftMetersPerSecond / kWheelRadius);
+    var rightAngularVelocity = RadiansPerSecond.of(speeds.rightMetersPerSecond / kWheelRadius);
+    leftDriveIO.setDriveVelocity(leftAngularVelocity);
+    rightDriveIO.setDriveVelocity(rightAngularVelocity);
+  }
+
+  /**
+   * Drives the robot with the given linear velocity and angular velocity.
+   *
+   * @param xSpeed Linear velocity in m/s.
+   * @param rot    Angular velocity in rad/s.
+   */
+  public void drive(double xSpeed, double rot) {
+    var wheelSpeeds = m_kinematics.toWheelSpeeds(new ChassisSpeeds(xSpeed, 0.0, rot));
+    setSpeeds(wheelSpeeds);
+  }
+
+  public DifferentialDriveWheelPositions getDifferentialDrivePositions() {
+    var leftPositionMeters = leftDriveIOInputs.position.in(Radians) * kWheelRadius;
+    var rightPositionMeters = rightDriveIOInputs.position.in(Radians) * kWheelRadius;
+    return new DifferentialDriveWheelPositions(leftPositionMeters, rightPositionMeters);
+  }
+
+  @Override
+  public void periodic() {
+    // Update the hardware IO inputs every period
+    leftDriveIO.updateInputs(leftDriveIOInputs);
+    rightDriveIO.updateInputs(rightDriveIOInputs);
+    gyroIO.updateInputs(gyroIOInputs);
+
+    // This logs the inputs during REAL or SIM mode, or replays them if in REPLAY
+    // mode
+    Logger.processInputs("Drivetrain/LeftDrive", leftDriveIOInputs);
+    Logger.processInputs("Drivetrain/RightDrive", rightDriveIOInputs);
+    Logger.processInputs("Drivetrain/Gyro", gyroIOInputs);
+
+    // Stop the robot if disabled
+    if (DriverStation.isDisabled()) {
+      leftDriveIO.setDriveOpenLoop(Volts.of(0));
+      rightDriveIO.setDriveOpenLoop(Volts.of(0));
+    }
+
+    // Update odometry, all signals are sampled together
+    var sampleTimestamps = leftDriveIOInputs.odometryTimestampsSec;
+    int sampleCount = sampleTimestamps.length;
+    for (int i = 0; i < sampleCount; i++) {
+      // The simulator abstraction for the gyro is tightly coupled to the Drivetrain
+      // subsytem, so it cannot be implemented in its own class, it must be here
+      var wheelPositions = getDifferentialDrivePositions();
+      if (gyroIOInputs.connected) {
+        // Real gyro rotation
+        rawGyroRotation = gyroIOInputs.yawPosition;
+      } else {
+        // Simulated gyro rotation
+        rawGyroRotation = rawGyroRotation
+            .plus(Rotation2d.fromRadians(m_kinematics.toTwist2d(lastWheelPositions, wheelPositions).dtheta));
+      }
+
+      // update last wheel positions for delta
+      lastWheelPositions = wheelPositions;
+
+      // update pose estimator
+      poseEstimator.updateWithTime(sampleTimestamps[i], rawGyroRotation, lastWheelPositions.leftMeters,
+          lastWheelPositions.rightMeters);
+    }
+
+    // Log the robot pose
+    Logger.recordOutput("Robot Pose", poseEstimator.getEstimatedPosition());
+  }
+}
--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,98 +4,141 @@
 
 package frc.robot;
 
-import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj2.command.Command;
+import org.littletonrobotics.junction.LogFileUtil;
+import org.littletonrobotics.junction.LoggedRobot;
+import org.littletonrobotics.junction.Logger;
+import org.littletonrobotics.junction.networktables.NT4Publisher;
+import org.littletonrobotics.junction.wpilog.WPILOGReader;
+import org.littletonrobotics.junction.wpilog.WPILOGWriter;
+import org.littletonrobotics.urcl.URCL;
+
+import edu.wpi.first.math.filter.SlewRateLimiter;
+import edu.wpi.first.wpilibj.Threads;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
-/**
- * The methods in this class are called automatically corresponding to each mode, as described in
- * the TimedRobot documentation. If you change the name of this class or the package after creating
- * this project, you must also update the Main.java file in the project.
- */
-public class Robot extends TimedRobot {
-  private Command m_autonomousCommand;
+public class Robot extends LoggedRobot {
+  private final XboxController m_controller = new XboxController(0);
+  private final Drivetrain m_drive;
 
-  private final RobotContainer m_robotContainer;
+  // Slew rate limiters to make joystick inputs more gentle; 1/3 sec from 0 to 1.
+  private final SlewRateLimiter m_speedLimiter = new SlewRateLimiter(3);
+  private final SlewRateLimiter m_rotLimiter = new SlewRateLimiter(3);
 
-  /**
-   * This function is run when the robot is first started up and should be used for any
-   * initialization code.
-   */
   public Robot() {
-    // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
-    // autonomous chooser on the dashboard.
-    m_robotContainer = new RobotContainer();
+    // Record metadata
+    Logger.recordMetadata("ProjectName", BuildConstants.MAVEN_NAME);
+    Logger.recordMetadata("BuildDate", BuildConstants.BUILD_DATE);
+    Logger.recordMetadata("GitSHA", BuildConstants.GIT_SHA);
+    Logger.recordMetadata("GitDate", BuildConstants.GIT_DATE);
+    Logger.recordMetadata("GitBranch", BuildConstants.GIT_BRANCH);
+    switch (BuildConstants.DIRTY) {
+      case 0:
+        Logger.recordMetadata("GitDirty", "All changes committed");
+        break;
+      case 1:
+        Logger.recordMetadata("GitDirty", "Uncomitted changes");
+        break;
+      default:
+        Logger.recordMetadata("GitDirty", "Unknown");
+        break;
+    }
+
+    // Set up data receivers & replay source
+    switch (Constants.currentMode) {
+      case REAL:
+        // Running on a real robot, log to a USB stick ("/U/logs")
+        Logger.addDataReceiver(new WPILOGWriter());
+        Logger.addDataReceiver(new NT4Publisher());
+        break;
+
+      case SIM:
+        // Running a physics simulator, log to NT
+        Logger.addDataReceiver(new NT4Publisher());
+
+        // Enable/disable this line to create logs from the simulator
+        // Logger.addDataReceiver(new WPILOGWriter());
+        break;
+
+      case REPLAY:
+        // Replaying a log, set up replay source
+        setUseTiming(false); // Run as fast as possible
+        String logPath = LogFileUtil.findReplayLog();
+        Logger.setReplaySource(new WPILOGReader(logPath));
+        Logger.addDataReceiver(new WPILOGWriter(LogFileUtil.addPathSuffix(logPath, "_sim")));
+        break;
+    }
+
+    // Initialize URCL
+    Logger.registerURCL(URCL.startExternal());
+
+    // Start AdvantageKit logger
+    Logger.start();
+
+    switch (Constants.currentMode) {
+      case REAL:
+        // Real robot, instantiate hardware IO implementations
+        m_drive = new Drivetrain(
+            new DriveSideIOSparkMax(true),
+            new DriveSideIOSparkMax(false),
+            new GyroIOAnalog());
+
+        break;
+
+      case SIM:
+        // Sim robot, instantiate physics sim IO implementations
+        m_drive = new Drivetrain(
+            new DriveSideIOSim(true),
+            new DriveSideIOSim(false),
+            new GyroIO() {
+            });
+        break;
+
+      default:
+        // Replayed robot, disable IO implementations
+        m_drive = new Drivetrain(
+            new DriveSideIOSim(true),
+            new DriveSideIOSim(false),
+            new GyroIO() {
+            });
+        break;
+    }
   }
 
-  /**
-   * This function is called every 20 ms, no matter the mode. Use this for items like diagnostics
-   * that you want ran during disabled, autonomous, teleoperated and test.
-   *
-   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
-   * SmartDashboard integrated updating.
-   */
+  /** This function is called periodically during all modes. */
   @Override
   public void robotPeriodic() {
-    // Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
-    // commands, running already-scheduled commands, removing finished or interrupted commands,
-    // and running subsystem periodic() methods.  This must be called from the robot's periodic
-    // block in order for anything in the Command-based framework to work.
+    // Switch thread to high priority to improve loop timing
+    Threads.setCurrentThreadPriority(true, 99);
+
+    // Runs the Scheduler. This is responsible for polling buttons, adding
+    // newly-scheduled commands, running already-scheduled commands, removing
+    // finished or interrupted commands, and running subsystem periodic() methods.
+    // This must be called from the robot's periodic block in order for anything in
+    // the Command-based framework to work.
     CommandScheduler.getInstance().run();
+
+    // Return to normal thread priority
+    Threads.setCurrentThreadPriority(false, 10);
   }
 
-  /** This function is called once each time the robot enters Disabled mode. */
   @Override
-  public void disabledInit() {}
-
-  @Override
-  public void disabledPeriodic() {}
-
-  /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
-  @Override
-  public void autonomousInit() {
-    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
-
-    // schedule the autonomous command (example)
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.schedule();
-    }
+  public void autonomousPeriodic() {
+    teleopPeriodic();
   }
 
-  /** This function is called periodically during autonomous. */
   @Override
-  public void autonomousPeriodic() {}
+  public void teleopPeriodic() {
+    // Get the x speed. We are inverting this because Xbox controllers return
+    // negative values when we push forward.
+    final var xSpeed = -m_speedLimiter.calculate(m_controller.getLeftY()) * Drivetrain.kMaxSpeed;
 
-  @Override
-  public void teleopInit() {
-    // This makes sure that the autonomous stops running when
-    // teleop starts running. If you want the autonomous to
-    // continue until interrupted by another command, remove
-    // this line or comment it out.
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.cancel();
-    }
+    // Get the rate of angular rotation. We are inverting this because we want a
+    // positive value when we pull to the left (remember, CCW is positive in
+    // mathematics). Xbox controllers return positive values when you pull to
+    // the right by default.
+    final var rot = -m_rotLimiter.calculate(m_controller.getRightX()) * Drivetrain.kMaxAngularSpeed;
+
+    m_drive.drive(xSpeed, rot);
   }
-
-  /** This function is called periodically during operator control. */
-  @Override
-  public void teleopPeriodic() {}
-
-  @Override
-  public void testInit() {
-    // Cancels all running commands at the start of test mode.
-    CommandScheduler.getInstance().cancelAll();
-  }
-
-  /** This function is called periodically during test mode. */
-  @Override
-  public void testPeriodic() {}
-
-  /** This function is called once when the robot is first started up. */
-  @Override
-  public void simulationInit() {}
-
-  /** This function is called periodically whilst in simulation. */
-  @Override
-  public void simulationPeriodic() {}
 }
