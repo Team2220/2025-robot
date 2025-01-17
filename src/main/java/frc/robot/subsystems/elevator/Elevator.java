d51611e1c0508d29981a3e17ed5a3e713e993253
Anvith <819178@apps.district196.org>
2025-01-16 19:50:39 -0600

    elevator motors

--- src/main/java/frc/robot/subsystems/elevator/Elevator.java
+++ src/main/java/frc/robot/subsystems/elevator/Elevator.java
@@ -1,29 +1,58 @@
 package frc.robot.subsystems.elevator;
 
-import com.ctre.phoenix6.configs.TalonFXConfiguration;
-import com.ctre.phoenix6.hardware.TalonFX;
-import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+
+import edu.wpi.first.units.Units;
+import frc.lib.devices.TalonFXWrapper;
+import static edu.wpi.first.units.Units.*;
 
 public class Elevator {
-    TalonFX elevatorMotorR;
-    TalonFX elevatorMotorL;
-    TalonFXConfiguration configR;
-    TalonFXConfiguration configL;
+    TalonFXWrapper elevatorMotorR;
+    TalonFXWrapper elevatorMotorL;
     double gearRatio;
+    double P;
+    double I;
+    double D;
 
     public Elevator() {
-        elevatorMotorR = new TalonFX(1); // on the right of the elevator (inverted)
-        elevatorMotorL = new TalonFX(2); // on the left of the elevator
-        gearRatio = 0; // change when we know the gear ratio
+        elevatorMotorR = new TalonFXWrapper(
+                1,
+                "Right Motor",
+                true,
+                NeutralModeValue.Brake,
+                1,
+                P,
+                I,
+                D,
+                RotationsPerSecondPerSecond.of(0),
+                RotationsPerSecond.of(0),
+                false,
+                false,
+                Rotations.of(0),
+                Rotations.of(0),
+                null,
+                Units.Seconds.of(1),
+                Units.Amps.of(75),
+                Units.RotationsPerSecond.of(1));
 
-        configR = new TalonFXConfiguration();
-        configL = new TalonFXConfiguration();
-
-        configR.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
-        configR.Feedback.SensorToMechanismRatio = gearRatio;
-        configL.Feedback.SensorToMechanismRatio = gearRatio;
-
-        elevatorMotorR.getConfigurator().apply(configR);
-        elevatorMotorL.getConfigurator().apply(configL);
+        elevatorMotorL = new TalonFXWrapper(
+                2,
+                "Left Motor",
+                false,
+                NeutralModeValue.Brake,
+                1,
+                P,
+                I,
+                D,
+                RotationsPerSecondPerSecond.of(0),
+                RotationsPerSecond.of(0),
+                false,
+                false,
+                Rotations.of(0),
+                Rotations.of(0),
+                null,
+                Units.Seconds.of(1),
+                Units.Amps.of(75),
+                Units.RotationsPerSecond.of(1));
     }
 }
