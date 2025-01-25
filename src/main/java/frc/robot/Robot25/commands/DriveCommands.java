1d6894420ea54a245f25ef4b5722692f44bf3355
notGriffin <104639986+scuyul@users.noreply.github.com>
2025-01-24 19:30:33 -0600

    yahooooo

--- src/main/java/frc/robot/Robot25/commands/DriveCommands.java
+++ src/main/java/frc/robot/Robot25/commands/DriveCommands.java
@@ -97,7 +97,7 @@ public class DriveCommands {
     return Commands.run(() -> {
       // Get linear velocity
       Translation2d linearVelocity =
-          getLinearVelocityFromJoysticks(xSupplier.getAsDouble(), ySupplier.getAsDouble());
+          getLinearVelocityFromJoysticks(-xSupplier.getAsDouble(), -ySupplier.getAsDouble());
 
       // Apply rotation deadband
       double omega = MathUtil.applyDeadband(omegaSupplier.getAsDouble(), DEADBAND);
