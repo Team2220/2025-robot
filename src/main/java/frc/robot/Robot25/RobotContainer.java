b187b05bb3f37f64d8832193c22e3ecde8ea5e7e
notGriffin <104639986+scuyul@users.noreply.github.com>
2025-01-30 20:25:38 -0600

    qpowkdkwqd

--- src/main/java/frc/robot/Robot25/RobotContainer.java
+++ src/main/java/frc/robot/Robot25/RobotContainer.java
@@ -43,9 +43,12 @@ import org.littletonrobotics.junction.networktables.LoggedDashboardChooser;
 import com.pathplanner.lib.auto.NamedCommands;
 
 /**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * This class is where the bulk of the robot should be declared. Since
+ * Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in
+ * the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of
+ * the robot (including
  * subsystems, commands, and button mappings) should be declared here.
  */
 public class RobotContainer extends frc.lib.RobotContainer {
@@ -53,8 +56,8 @@ public class RobotContainer extends frc.lib.RobotContainer {
   private final Drive drive;
 
   // Drive simulation
-  private static final SwerveDriveSimulation driveSimulation =
-      new SwerveDriveSimulation(Drive.MAPLE_SIM_CONFIG, SimConstants.SIM_INITIAL_FIELD_POSE);
+  private static final SwerveDriveSimulation driveSimulation = new SwerveDriveSimulation(Drive.MAPLE_SIM_CONFIG,
+      SimConstants.SIM_INITIAL_FIELD_POSE);
 
   // Controller
   private final CommandXboxController DriverController = new CommandXboxController(0);
@@ -63,13 +66,15 @@ public class RobotContainer extends frc.lib.RobotContainer {
   // Dashboard inputs
   private final LoggedDashboardChooser<Command> autoChooser;
 
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  /**
+   * The container for the robot. Contains subsystems, OI devices, and commands.
+   */
   public RobotContainer() {
     super(driveSimulation);
 
     // Check for valid swerve config
-    var modules = new SwerveModuleConstants[] {DriveConstants.FrontLeft, DriveConstants.FrontRight,
-        DriveConstants.BackLeft, DriveConstants.BackRight};
+    var modules = new SwerveModuleConstants[] { DriveConstants.FrontLeft, DriveConstants.FrontRight,
+        DriveConstants.BackLeft, DriveConstants.BackRight };
     for (var constants : modules) {
       if (constants.DriveMotorType != DriveMotorArrangement.TalonFX_Integrated
           || constants.SteerMotorType != SteerMotorArrangement.TalonFX_Integrated) {
@@ -98,18 +103,19 @@ public class RobotContainer extends frc.lib.RobotContainer {
 
       default:
         // Replayed robot, disable IO implementations
-        drive = new Drive(new GyroIO() {}, new ModuleIO() {}, new ModuleIO() {}, new ModuleIO() {},
-            new ModuleIO() {});
+        drive = new Drive(new GyroIO() {
+        }, new ModuleIO() {
+        }, new ModuleIO() {
+        }, new ModuleIO() {
+        },
+            new ModuleIO() {
+            });
         break;
     }
-
-
 
     NamedCommands.registerCommand("Test", drive.NullCommand(1));
     NamedCommands.registerCommand("ToggleCoast", drive.toggleCoastCommand());
     NamedCommands.registerCommand("Null", drive.NullCommand(2));
-
-
 
     // Set up auto routines
     autoChooser = new LoggedDashboardChooser<>("Auto Choices", AutoBuilder.buildAutoChooser());
@@ -136,9 +142,11 @@ public class RobotContainer extends frc.lib.RobotContainer {
   }
 
   /**
-   * Use this method to define your button->command mappings. Buttons can be created by
+   * Use this method to define your button->command mappings. Buttons can be
+   * created by
    * instantiating a {@link GenericHID} or one of its subclasses
-   * ({@link edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a
+   * ({@link edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then
+   * passing it to a
    * {@link edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
   private void configureButtonBindings() {
@@ -149,9 +157,8 @@ public class RobotContainer extends frc.lib.RobotContainer {
     DoubleSupplier ySupplier = () -> !SimConstants.IS_MAC ? -DriverController.getRightY()
         : -DriverController.getLeftTriggerAxis();
     DoubleSupplier omegaSupplier = () -> -DriverController.getLeftX();
-    BooleanSupplier slowModeSupplier =
-        () -> !SimConstants.IS_MAC ? DriverController.getRightTriggerAxis() > 0.5
-            : DriverController.getRightX() > 0.0;
+    BooleanSupplier slowModeSupplier = () -> !SimConstants.IS_MAC ? DriverController.getRightTriggerAxis() > 0.5
+        : DriverController.getRightX() > 0.0;
 
     // Default command, normal field-relative drive
     drive.setDefaultCommand(
