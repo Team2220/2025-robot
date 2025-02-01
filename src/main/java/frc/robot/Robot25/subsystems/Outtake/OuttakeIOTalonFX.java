d5293ccc28809b7460d7a027d146e86300af17c7
0vzer <uyennguyenxdao@gmail.com>
2025-02-01 15:23:14 -0600

    Update OuttakeIOTalonFX.java

--- src/main/java/frc/robot/Robot25/subsystems/Outtake/OuttakeIOTalonFX.java
+++ src/main/java/frc/robot/Robot25/subsystems/Outtake/OuttakeIOTalonFX.java
@@ -1,18 +1,9 @@
 package frc.robot.Robot25.subsystems.outtake;
 
-import static edu.wpi.first.units.Units.Rotations;
-import static edu.wpi.first.units.Units.RotationsPerSecond;
-import static edu.wpi.first.units.Units.RotationsPerSecondPerSecond;
 import static edu.wpi.first.units.Units.Volts;
-
 import com.ctre.phoenix6.signals.NeutralModeValue;
-import edu.wpi.first.units.Units;
-import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.Voltage;
 import frc.lib.devices.TalonFXWrapper;
-import frc.lib.devices.TalonFXWrapper.FollowerConfig;
-import frc.robot.Robot25.subsystems.outtake.OuttakeIO;
-import frc.robot.Robot25.subsystems.outtake.OuttakeIO.OuttakeIOInputs;
 
 public class OuttakeIOTalonFX implements OuttakeIO {
   TalonFXWrapper outtakeTalonFX;
